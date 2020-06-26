3812a4f36392f4d3ddf360d57b9fc2f56b0bfad1
bryansouza <bryancjsouza@gmail.com>
2020-06-25 23:36:49 -0700

    added image loading worker

--- js/page-logic.js
+++ js/page-logic.js
@@ -20,3 +20,38 @@ document.addEventListener('DOMContentLoaded', function() {
     var parallaxEl = document.querySelectorAll('.parallax');
     var para = M.Parallax.init(parallaxEl);
 });
+
+// Image Loader logic
+const ImageLoaderWorker = new Worker('/js/workers/image-loader.worker.js')
+const imgElements = document.querySelectorAll('img[data-src]')
+
+
+// We should attach the listener before we pass image URLs to the web worker
+// to catch messages sent prior to the event being attached
+ImageLoaderWorker.addEventListener('message', event => {
+    // Get the message data from the event
+    const imageData = event.data
+
+    // Select the original element for this image
+    const imageElement = document.querySelectorAll(`img[data-src='${imageData.imageURL}']`)
+
+    // We can use the `Blob` as an image source
+    // We just need to convert it to an object URL first
+    const objectURL = URL.createObjectURL(imageData.blob)
+
+    // Once the image is loaded, cleanup memory
+    imageElement.onload = () => {
+        // Remove the original `data-src` attribute to make sure we don't
+        // accidentally pass this image to the worker again in the future
+        imageElement.removeAttribute('data-src')
+
+        // Also revoke the object URL now that it's been used to cleanup
+        URL.revokeObjectURL(objectURL)
+    }
+    imageElement[0].setAttribute('src', objectURL)
+})
+
+imgElements.forEach(imageElement => {
+    const imageURL = imageElement.getAttribute('data-src')
+    ImageLoaderWorker.postMessage(imageURL)
+})
